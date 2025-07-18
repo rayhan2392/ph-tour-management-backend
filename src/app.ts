61050fd5e8367f3fe176691adf306535246cf5dc
rayhan2392 <rkshawnso@gmail.com>
2025-07-18 23:18:01 +0600

    create user api created

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,12 @@
-import express, { Request, Response } from 'express'
+import express, { Request, Response} from 'express'
+import cors from 'cors'
+import { UserRoute } from './app/modules/user/user.route';
 
 const app = express();
+app.use(express.json());
+app.use(cors())
+
+app.use('/api/v1',UserRoute)
 
 app.get('/', (req: Request, res: Response) => {
     res.status(200).json({
---
+++ src/app/modules/user/user.controller.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from "express";
+import { User } from "./user.model";
+import httpStatus from "http-status-codes"
+
+const createUser = async(req:Request,res:Response)=>{
+    
+try {
+   const {name,email}= req.body
+   const user = await User.create({
+    name,
+    email
+   })
+   res.status(httpStatus.CREATED).json({
+    user,
+    message:'User created successfully'
+   })
+} catch (error) {
+    console.log(error)
+    res.status(httpStatus.BAD_REQUEST).json({
+        message:`something went wrong ${error}`
+    })
+}
+}
+
+export const UserControllers= {
+   createUser
+}
