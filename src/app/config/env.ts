90c59864924923e030c176102c8d97ca10a8ff85
rayhan2392 <rkshawnso@gmail.com>
2025-07-20 22:34:55 +0600

    env updated

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -8,13 +8,15 @@ interface EnvConfig {
     NODE_ENV: 'development' | 'production',
     JWT_ACCESS_SECRET:string,
     JWT_ACCESS_EXPIRES:string,
+    JWT_REFRESH_SECRET:string,
+    JWT_REFRESH_EXPIRES:string,
     BCRYPT_SALT_ROUND:string,
     SUPER_ADMIN_EMAIL:string,
-    SUPER_ADMIN_PASSWOERD:string
+    SUPER_ADMIN_PASSWOERD:string,
 }
 
 const loadEnvVariables = (): EnvConfig => {
-    const requiredEnvVariables: string[] = ['PORT', 'DB_URL', 'NODE_ENV','JWT_ACCESS_SECRET','JWT_ACCESS_EXPIRES','BCRYPT_SALT_ROUND', 'SUPER_ADMIN_EMAIL', 'SUPER_ADMIN_PASSWOERD']
+    const requiredEnvVariables: string[] = ['PORT', 'DB_URL', 'NODE_ENV','JWT_ACCESS_SECRET','JWT_ACCESS_EXPIRES','BCRYPT_SALT_ROUND', 'SUPER_ADMIN_EMAIL', 'SUPER_ADMIN_PASSWOERD', 'JWT_REFRESH_SECRET', 'JWT_REFRESH_EXPIRES']
     requiredEnvVariables.forEach(key => {
         if (!process.env[key]) {
             throw new Error(`missing enviroment variable ${key}`)
@@ -28,7 +30,9 @@ const loadEnvVariables = (): EnvConfig => {
         JWT_ACCESS_EXPIRES:process.env.JWT_ACCESS_EXPIRES as string,
         BCRYPT_SALT_ROUND:process.env.BCRYPT_SALT_ROUND as string,
         SUPER_ADMIN_EMAIL:process.env.SUPER_ADMIN_EMAIL as string,
-        SUPER_ADMIN_PASSWOERD:process.env.SUPER_ADMIN_PASSWOERD as string
+        SUPER_ADMIN_PASSWOERD:process.env.SUPER_ADMIN_PASSWOERD as string,
+        JWT_REFRESH_SECRET:process.env.JWT_REFRESH_SECRET as string,
+        JWT_REFRESH_EXPIRES:process.env.JWT_REFRESH_EXPIRES as string
     }
 }
 
