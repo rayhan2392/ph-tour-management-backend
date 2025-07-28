c271060f926222f9ebf213e6c7ecbc01bf1cfad0
rayhan2392 <rkshawnso@gmail.com>
2025-07-29 03:12:38 +0600

    payment gateway integrated with ssl commerz

--- src/app/modules/booking/booking.service.ts
+++ src/app/modules/booking/booking.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import AppError from "../../errorHelpers/AppError";
 import { User } from "../user/user.model";
 import { BOOKING_STATUS, IBooking } from "./booking.interface"
@@ -6,6 +7,8 @@ import { Booking } from "./booking.model";
 import { Payment } from "../payment/payment.model";
 import { PAYMENT_STATUS } from "../payment/payment.interface";
 import { Tour } from "../tour/tour.model";
+import { ISSLCommerz } from "../sslCommerz/sslCommerz.interface";
+import { SSLService } from "../sslCommerz/sssCommerz.service";
 
 const getTransactionId = () => {
     return `tran_${Date.now()}_${Math.floor(Math.random() * 1000)}`
@@ -36,7 +39,7 @@ const createBooking = async (payload: Partial<IBooking>, userId: string) => {
             user: userId,
             status: BOOKING_STATUS.PENDING,
             ...payload,
-        }],{session})
+        }], { session })
         //create payment for respective booking
         const payment = await Payment.create([{
             booking: booking[0]._id,
@@ -44,22 +47,46 @@ const createBooking = async (payload: Partial<IBooking>, userId: string) => {
             transactionId: transactionId,
             amount: amount
 
-        }],{session})
+        }], { session })
         //update the booking wiht payment information
         const updatedBooking = await Booking
             .findByIdAndUpdate(
                 booking[0]._id,
                 { payment: payment[0]._id },
-                { new: true, runValidators: true,session }
+                { new: true, runValidators: true, session }
             )
-            .populate("user", "name email")
+            .populate("user", "name email address phone")
             .populate("tour", "title costFrom")
             .populate("payment", "amount status")
 
-            await session.commitTransaction()
-            session.endSession()
+        //initiate payment with ssl commerz
 
-        return updatedBooking;
+        const userAddress = (updatedBooking?.user as any).address
+        const userEmail = (updatedBooking?.user as any).email
+        const userPhone = (updatedBooking?.user as any).phone
+        const userName = (updatedBooking?.user as any).name
+
+        const sslPayload: ISSLCommerz = {
+            address: userAddress,
+            email: userEmail,
+            phoneNumber: userPhone,
+            transactionId: transactionId,
+            amount: amount,
+            name: userName
+
+        }
+
+        const sslPayment = await SSLService.sslPaymentInit(sslPayload)
+        console.log(sslPayment)
+
+        await session.commitTransaction()
+        session.endSession()
+
+        return {
+            paymentURL: sslPayment.GatewayPageURL,
+            booking: updatedBooking
+
+        }
 
     } catch (error) {
         await session.abortTransaction() //rollback
